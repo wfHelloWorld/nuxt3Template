f689e8e7711d9d4e5ae7d888b43cf0640cc2f714
Mac <wf@MacdeMac-Studio.local>
2023-09-14 20:46:21 +0800

    add content

---
+++ components/content/card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        card component
+    </div>
+    <slot></slot>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ content/index.md
@@ -0,0 +1,7 @@
+# hello
+
+::card
+这个是 card 的 slot 内容
+::
+
+:noSlotCard
