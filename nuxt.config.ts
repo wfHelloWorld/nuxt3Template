3a76a19385e875bddc2b43f1b737d940a349a07d
wf <374006704@qq.com>
2023-09-18 11:10:55 +0800

    add pinia scs vueuse PWA ElementPlus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxt/content',
+    '@pinia/nuxt',
+    '@element-plus/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+    '@vite-pwa/nuxt'
+  ],
+  css: [
+    // 导入样式
+    // '你的路径' 
+  ],
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Vite App',
+      short_name: 'Vite App',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      // 如果大家有很大的资源文件，wasm bundle.js
+      globPatterns: ['**/*.*']
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module'
+    }
+  }
 })
