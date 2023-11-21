86729dd313de940d715f67b57d0809e29f0e0603
wf <374006704@qq.com>
2023-11-21 21:17:53 +0800

    server 文件下的 api 请求模版

--- server/api/test.ts
+++
@@ -1,7 +0,0 @@
-// 两个 ESLint 生效
-// 两个错误
-
-// if (false){
-//     /* @ts-ignore */
-//     console.log('hello world')
-// }
---
+++ server/routes/mock/hello.post.ts
@@ -0,0 +1,1 @@
+export default defineEventHandler(() => 'Post Hello World')
