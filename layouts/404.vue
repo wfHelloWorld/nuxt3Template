cc469d7560ab68ad8deb4f53cb1e03d8cf997e27
wf <374006704@qq.com>
2023-11-23 20:40:45 +0800

    server中间件

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,10 +1,12 @@
+import { HelloWorld } from '../.nuxt/components';
 <template>
     <div>
+        <NuxtPage></NuxtPage>
         <h1>404</h1>
         <p>路径错误,没有找到页面</p>
         <el-button @click="router.back">返回</el-button>
-
     </div>
+    <HelloWorld></HelloWorld>
 </template>
 
 <script setup lang="ts">
